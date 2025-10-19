8d038719f5ebd5d0f7f883ab960736ace18f3105
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-10-18 22:56:31 -0500

    dim_product started, start_date, end_date, status and english_product_name error are missing

--- notebooks/connection.py
+++ notebooks/connection.py
@@ -6,15 +6,20 @@ def connect(schema):
         config = yaml.safe_load(f)
         config_co = config['CO_SA']
         config_etl = config['ETL_PRO']
+        config_etl_or=config['ETL_PRO_OR']
 
     # Construct the database URL
     url_co = (f"{config_co['drivername']}://{config_co['user']}:{config_co['password']}@{config_co['host']}:"
             f"{config_co['port']}/{config_co['dbname']}")
     url_etl = (f"{config_etl['drivername']}://{config_etl['user']}:{config_etl['password']}@{config_etl['host']}:"
             f"{config_etl['port']}/{config_etl['dbname']}")
+
+    url_etl_or = (f"{config_etl_or['drivername']}://{config_etl_or['user']}:{config_etl_or['password']}@{config_etl_or['host']}:"
+            f"{config_etl_or['port']}/{config_etl_or['dbname']}")
     # Create the SQLAlchemy Engine
     co_oltp = create_engine(url_co)
     etl_conn = create_engine(url_etl)
+    etl_conn_or=create_engine(url_etl_or)
 
     inspector2=inspect(co_oltp)
     inspector = inspect(etl_conn)
@@ -29,4 +34,4 @@ def connect(schema):
                 for key, val in sql.items():
                     conn.execute(text(val))
             conn.commit()
-    return co_oltp,etl_conn 
+    return co_oltp,etl_conn,etl_conn_or
