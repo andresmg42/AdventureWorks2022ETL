971398adbc5b1abfdcbbab0979ad382e90a14c09
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-11-09 10:30:50 -0500

    Convirtiendo el Schema a constante y devolviendo connection a estado original
    Ahora hay una constante para el SCHEMA en el flujo ETL y las configuraciones de connection vuelven a su forma original

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from etl.load import load_to_dw
 from connection import connect
 from utils.model_loader import ModelRegistry
 
+SCHEMA = 'dw'
+
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 100)
 
@@ -26,36 +28,36 @@ def main():
     # Proceso DimSalesTerritory (independiente)
     df_sales_territory_raw = extract_sales_territory(co_oltp, 'sales')
     df_sales_territory_final = transform_sales_territory(df_sales_territory_raw)
-    load_to_dw(df_sales_territory_final,'dim_sales_territory','dw',etl_conn)
+    load_to_dw(df_sales_territory_final,'dim_sales_territory', SCHEMA, etl_conn)
 
     # Proceso DimCurrency (independiente)
     df_currency_raw = extract_currency(co_oltp)
     df_currency_final = transform_currency(df_currency_raw)
-    load_to_dw(df_currency_final, 'dim_currency', 'dw', etl_conn)
+    load_to_dw(df_currency_final, 'dim_currency', SCHEMA, etl_conn)
 
     # Proceso DimGeography (depende de DimSalesTerritory)
     df_geography_raw = extract_geography(co_oltp)
     df_geography_final = transform_geography(df_geography_raw, etl_conn, model_registry)
-    load_to_dw(df_geography_final, 'dim_geography', 'dw', etl_conn)
+    load_to_dw(df_geography_final, 'dim_geography', SCHEMA, etl_conn)
 
     # Proceso DimDate (independiente)
     df_date_final = transform_date()
-    load_to_dw(df_date_final, 'dim_date', 'dw', etl_conn)
+    load_to_dw(df_date_final, 'dim_date', SCHEMA, etl_conn)
 
     # Proceso DimPromotion (independiente)
     df_promotion_raw = extract_promotion(co_oltp, 'sales')
     df_promotion_final = transform_promotion(df_promotion_raw, model_registry)
-    load_to_dw(df_promotion_final, 'dim_promotion', 'dw', etl_conn)
+    load_to_dw(df_promotion_final, 'dim_promotion', SCHEMA, etl_conn)
 
     # Proceso DimProductCategory (independiente)
     df_product_category_raw = extract_product_category(co_oltp, 'production')
     df_product_category_final = transform_product_category(df_product_category_raw, model_registry)
-    load_to_dw(df_product_category_final, 'dim_product_category', 'dw', etl_conn)
+    load_to_dw(df_product_category_final, 'dim_product_category', SCHEMA, etl_conn)
 
     # Proceso DimProductSubCategory (depende de DimProductCategory)
     df_product_subcategory_raw = extract_product_subcategory(co_oltp, 'production')
     df_product_subcategory_final = transform_product_subcategory(df_product_subcategory_raw, etl_conn,model_registry)
-    load_to_dw(df_product_subcategory_final, 'dim_product_subcategory', 'dw', etl_conn)
+    load_to_dw(df_product_subcategory_final, 'dim_product_subcategory', SCHEMA, etl_conn)
 
     # Proceso DimProduct (depende de DimProductSubCategory)
     df_product_raw = extract_product(co_oltp, 'production')
@@ -75,12 +77,12 @@ def main():
         etl_conn,
         model_registry
     )
-    load_to_dw(df_product_raw_final, 'dim_product', 'dw', etl_conn)
+    load_to_dw(df_product_raw_final, 'dim_product', SCHEMA, etl_conn)
 
     # Proceso DimCustomer (depende de DimGeography)
     df_customer_raw = extract_customer(co_oltp)
     df_customer_final = transforms_customer(df_customer_raw, etl_conn, model_registry)
-    load_to_dw(df_customer_final, 'dim_customer', 'dw', etl_conn)
+    load_to_dw(df_customer_final, 'dim_customer', SCHEMA, etl_conn)
 
 
 if __name__ == "__main__":
